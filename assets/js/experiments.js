03ed4303db674a40981a1f2bb1fd9039752d225a
Callum Neideck <cal_neideck@hotmail.com>
2017-01-18 11:17:58 +1100

    MOD

--- assets/js/experiments.js
+++ assets/js/experiments.js
@@ -1,6 +1,12 @@
 $(function() {
-  var height = new p5().windowHeight;
+  var tempp5 = new p5();
+  var height = tempp5.windowHeight;
   $('#oct').height(height);
   $('#dec').height(height);
   $('#jan').height(height);
+  
+  tempp5.draw = function() {
+    remove();
+  }
 });
+
