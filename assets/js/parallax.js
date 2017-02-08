3bf9eea222b9477fe966b986d84f91f15094b607
Callum Neideck <cal_neideck@hotmail.com>
2017-02-08 14:38:41 +1100

    MOD

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,31 +1,28 @@
 var parallaxElements = $('.parallax-img');
-var parallaxQuantity = parallaxElements.length;
 var scale = 0.9;
 
-$(window).on('scroll', function ()
-{
+$(window).on('scroll', function () {
   parallax();
 });
 
-$(window).on('resize', function ()
-{
+$(window).on('resize', function () {
   parallax();
 });
 
 function parallax() {
-  for (var i = 0; i < parallaxQuantity; i++)
-  {
+  for (var i = 0; i < parallaxElements.length; i++) {
     var currentElement = parallaxElements.eq(i);
     var parallax = currentElement.parent();
     var height = currentElement.height();
     var topOfParallax = parallax.offset().top;
-    var windowBottom = $(window).scrollTop() + $(window).height();
-    var pct = (windowBottom - topOfParallax) / ($(window).height() + parallax.height()) * scale;
+    var windowBottom = $(window).scrollTop() + window.innerHeight;
+    var pct = (windowBottom - topOfParallax) / (window.innerHeight + parallax.height()) * scale;
     var initialOffset = parallax.height() - height;
     var margin = initialOffset * (1 - pct);
+    var translateY = 'translateY(' + margin + 'px)';
     
     currentElement.css({
-      'margin-top': margin + "px"
+      'transform': translateY
     });
   }
 }
