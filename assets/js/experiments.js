73240adeb393a12fea7fa17c65db89ca66cc42b0
Callum Neideck <cal_neideck@hotmail.com>
2017-02-15 14:07:42 +1100

    MOD

--- assets/js/experiments.js
+++ assets/js/experiments.js
@@ -9,44 +9,42 @@ $(window).resize(function() {
 });
 
 function imageGrid() {
-  var numOfImages = $(".grid").length;
-  var w = window.innerWidth;
-  
-  if (w >= 1400) {
-    console.log(6);
-    if (numOfImages % 6 != 0)
-      resizeGrid(numOfImages, 6, "calc(16.66% - 4.16px)");
-  }
-  else if (w >= 1000) {
-    console.log(5);
-    if (numOfImages % 5 != 0)
-      resizeGrid(numOfImages, 5, "calc(20% - 4px)");
-  }
-  else if (w >= 768) {
-    console.log(4);
-    if (numOfImages % 4 != 0)
-      resizeGrid(numOfImages, 4, "calc(25% - 3.75px)");
-  }
-  else {
-    console.log(2);
-    if (numOfImages % 2 != 0)
-      resizeGrid(numOfImages, 2, "calc(50% - 2.5px)");
-  }
+  resizeGrid();
 }
 
-function resizeGrid(numOfImages, gridSize, calc) {
-  for (var i = 0; i < numOfImages; i++)
+function resizeGrid(numOfImages) {
+  var numOfImages = $(".grid").length;
+  var area = (window.innerHeight - 160) * window.innerWidth;
+  var imageSize = Math.ceil(Math.sqrt(area / numOfImages));
+  var gridSize = Math.ceil(window.innerWidth / imageSize);
+  if (window.innerWidth < 768)
+    gridSize = 2;
+  
+  var calc = "calc(" + 100 / gridSize + "% - " + (gridSize - 1) * 5 / gridSize + "px)";
+    console.log(calc);
+  
+  for (var i = 0; i < numOfImages; i++) {
     $(".grid").eq(i).css({
-      'margin-right': '5px'
+      'margin-right': '5px',
+      'width': calc
     });
+  }
   
   var numToMove = gridSize - numOfImages % gridSize;
+  if (numToMove == gridSize)
+    numToMove = 0;
+  
   for (var i = 0; i < gaps.length; i++)
     gaps[i].remove();
   
   gaps = [];
   for (var i = 0; i < numToMove; i++) {
-    var elem = $(".grid").eq(Math.floor((Math.random() * numOfImages * 0.8))).after("<div />");
+    var elem;
+    if (window.innerWidth < 768)
+      elem = $(".grid").eq(numOfImages - 1).after("<div />");
+    else
+      elem = $(".grid").eq(Math.floor((Math.random() * numOfImages * 0.8))).after("<div />");
+    
     gaps[i] = elem.next();
     gaps[i].css({
       'display': 'inline-block',
